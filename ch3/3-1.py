ff33b558cd9f1dd7e854e45851852a4994f80efa
Lotus D <DingLQ@protonmail.com>
2020-03-07 14:52:08 +0800

    ch3

---
+++ ch3/3-1.py
@@ -0,0 +1,3 @@
+names = ['Jack', 'Tom']
+print(names[0])
+print(names[1])
---
+++ ch3/3-2.py
@@ -0,0 +1,5 @@
+names = ['Jack', 'Tom']
+message1 = f"Hello, {names[0]}!"
+message2 = f"Hello, {names[1]}!"
+print(message1)
+print(message2)
